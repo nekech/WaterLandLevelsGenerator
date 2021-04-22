d44852537aa69f62ac224190de44d65f232cd734
Nikolay Protasov <nikolay.protasov@intel.com>
2021-04-22 19:23:07 +0300

    fix memcpy

---
+++ build/GenerateLevels/CMakeFiles/GenerateLevels.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/Game.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/Game.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/GameCell.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/GameCell.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/GameField.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/GameField.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/GenerateLevels.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/GenerateLevels.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/GreedyPlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/GreedyPlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/GreedyPlayer2.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/GreedyPlayer2.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/InteractivePlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/InteractivePlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/MCTSNode.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/MCTSNode.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/MCTSPlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/MCTSPlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/MCTSTree.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/MCTSTree.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/MiniMaxModifiedPlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/MiniMaxModifiedPlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/MiniMaxPlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/MiniMaxPlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/RandomPlayer.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/RandomPlayer.cpp.o"
+  "/home/lab_vauser/My/WaterLandLevelsGenerator/GenerateLevels/Step.cpp" "/home/lab_vauser/My/WaterLandLevelsGenerator/build/GenerateLevels/CMakeFiles/GenerateLevels.dir/Step.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "UNICODE"
+  "_CONSOLE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/GenerateLevels/CMakeFiles/GenerateLevels.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenerateLevels.dir/Game.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/GameCell.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/GameField.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/GenerateLevels.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/GreedyPlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/GreedyPlayer2.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/InteractivePlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/MCTSNode.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/MCTSPlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/MCTSTree.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/MiniMaxModifiedPlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/MiniMaxPlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/RandomPlayer.cpp.o"
+  "CMakeFiles/GenerateLevels.dir/Step.cpp.o"
+  "GenerateLevels"
+  "GenerateLevels.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenerateLevels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
